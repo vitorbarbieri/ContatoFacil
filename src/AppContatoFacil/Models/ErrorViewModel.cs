09fd33b2845a6817626af7fb53d231364ba8dd73
vitorbarbieri <vitor.bsilva@ambevtech.com.br>
2025-06-10 15:52:34 -0300

    Criação do projeto ContatoFacil

---
+++ src/AppContatoFacil/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AppContatoFacil.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
